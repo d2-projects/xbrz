f8384c6a558934e7776dc6eb56f5f9bb7a4e9e35
FairyEver <1711467488@qq.com>
2020-07-04 01:36:05 +0800

    feat: :sparkles: babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,24 @@ const TerserPlugin = require('terser-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'xbrz.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'xbrz',
     libraryTarget: 'umd'
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin()
